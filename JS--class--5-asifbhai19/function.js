c24c0640cada1a6032f42c6fbd6867d70f1e2c15
Jidne007 <163674890+Jidne007@users.noreply.github.com>
2024-05-22 23:37:39 +0600

    Add files via upload

---
+++ JS--class--5-asifbhai19/function.js
@@ -0,0 +1,106 @@
+//FUNCTION 
+{
+function fun1(){
+  console.log('Jidne' + ' ' + 'Bhuiyan');
+}
+
+fun1();
+fun1();
+fun1();
+fun1();
+fun1();
+
+}
+
+
+//
+
+{
+  function fun2(){
+   console.log('Nusaiba')
+  };
+
+  fun2();
+}
+
+
+//
+{
+let a ='jide';
+let b = 'is a good student';
+
+function fun2(){
+     console.log(a+' '+b)
+}
+
+fun2();
+}
+
+//
+
+{
+
+  let a=2;
+  let b=3;
+
+  function fun3(){
+      if(a<b){
+           console.log('Eyta Shothik ache karon a=2')
+      } else{
+        console.log('syntax error karon b=3')
+      }
+  }
+
+  fun3();
+}
+
+//
+
+
+
+{
+
+  let a = 'jidne';
+  let b ='is a Web-Devloper';
+
+  function fun5() {
+
+    let name;
+    name = a+' ' + b;  
+    console.log(name);
+  }
+
+  fun5();
+}
+
+//
+
+{
+ 
+  const a = 'mehrab';
+  const b ='is a Doctor';
+
+  function fun5() {
+
+   const name = a+' ' + b;  
+    console.log(name);
+}
+
+fun5();
+
+}
+
+//
+
+
+{
+  let a ='musfiq';
+  let b=4;
+
+  function fun6(){
+    let output =a+b;
+    console.log(output);
+  }
+
+  fun6();
+}
